e495ce6435bfa6cb59683eb1a442d842aee398da
Batai2509 <88811070+Batai2509@users.noreply.github.com>
2021-08-12 11:00:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc : 'Ngại gì ko dùng messenger nhắn tin ngay 😊 .' ,
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/BA.tAi.dz.tv ' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
